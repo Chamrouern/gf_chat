b0deb168d328ef5b0def8cfc451ba37d1b4c647f
Leanghong <leanghong.oeng@r-up.jp>
2016-09-01 09:14:32 +0700

    home page and login and register
    - home page
    - menu bar
    - loging and register
    - chat box
    - post page
    - user profile and sing up dropdown

--- public/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-var ref = new Firebase('https://gf-chat.firebaseio.com/players');
-ref.on("value", function(snapshot) {
-	snapshot.forEach(function(data){
-		console.log("The " + data.key() + " dinosaur's score is " + data.val());
-	}); 	
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
