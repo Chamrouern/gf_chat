acbc6409c08dc39a4ed674cd358b1c5f5cf1b759
chamroeurn <chamroeurn.duch@gmail.com>
2016-09-08 11:34:11 +0700

    Register process

--- public/js/app.js
+++ public/js/app.js
@@ -8,24 +8,54 @@
   firebase.initializeApp(config);
 
 var app = angular.module("chatApp", ["firebase",'ui.router']);
-app.config(function ($stateProvider, $urlRouterProvider) {  
+app.config(function ($stateProvider, $urlRouterProvider) { 
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'views/post/home.html',
+        resolve: {
+           requireNoAuth : function(Auth,$state){ 
+           return Auth.$requireSignIn().then(function(auth){
+            $state.go('gift');
+          },function(error){
+            return;
+          });
+        }
+      }
       })
       .state('register', {
         url: '/register',
         controller: 'AuthCtrl as authCtrl',
         templateUrl: 'views/auth/register.html',
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+           requireNoAuth : function(Auth,$state){ 
+           return Auth.$requireSignIn().then(function(auth){
+            $state.go('gift');
+        },function(error){
+            return;
+          });
+        }
+      }
       })
       .state('login', {
         url: '/login',
         templateUrl: 'views/auth/login.html',
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+           requireNoAuth : function(Auth,$state){   
+           return Auth.$requireSignIn().then(function(auth){
+            $state.go('gift');
+          },function(error){
+            return;
+          });
+        }
+      }
       })
-          .state('gift', {
+      .state('gift', {
         url: '/gift',
-        templateUrl: 'views/post/gift.html',
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'views/post/gift.html'
       })
            .state('freebie', {
         url: '/freebie',
